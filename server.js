f382bcde10c7a1d2964eb052caaf901660c982c9
King Elisha <ek.chibueze@gmail.com>
2020-02-24 19:08:31 +0100

    basic setup

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 const { promisify } = require("util");
 
 const server = express();
 const readFile = promisify(fs.readFile);
 
-server.engine("simple", (path, options, callback) => {
-  readFile(path)
-    .then(buffer => {
-      let renderedStr = buffer.toString();
-      const variables = /{{.*}}/.exec(renderedStr);
-      console.log(variables);
-      return callback(null, renderedStr);
-    })
-    .catch(err => callback(err, null));
+server.engine("eng.html", (path, options, callback) => {
+  return callback(null, "Hello");
+  //   readFile(path)
+  //     .then(buffer => {
+  //       let renderedStr = buffer.toString();
+  //       const variables = /{{.*}}/.exec(renderedStr);
+  //       console.log(variables);
+  //       return callback(null, renderedStr);
+  //     })
+  //     .catch(err => callback(err, null));
 });
 
-server.set("views", "views");
-server.set("view engine", "simple");
+server.set("views", path.join(__dirname, "views"));
+server.set("view engine", "eng.html");
 
 server.get("/", (req, res) => {
-  res.render("index", { name: "king", email: "mail@mail.com" });
+  return res.render("index", { name: "king", email: "mail@mail.com" });
 });
 
 server.listen(8000, () => console.log("Server running..."));
