f8308a8faf4b501cc19c2dab9d9061152a213118
King Elisha <ek.chibueze@gmail.com>
2020-02-26 19:38:57 +0100

    simple for loop

--- server.js
+++ server.js
@@ -23,6 +23,32 @@ server.engine("eng.html", (path, options, callback) => {
           );
       });
 
+      const forLoop = new RegExp(/{{for (\w+) in (\w+)}}(.*){{endfor}}/g).exec(
+        JSON.stringify(renderedStr)
+      );
+      const forLoopVars = forLoop.slice(0, forLoop.length);
+      const forLoopArr = options[forLoopVars[2]];
+      if (forLoopArr) {
+        let forLoopStrResult = "";
+        const forLoopContent = forLoopVars[3]
+          .replace(/\\r\\n(.*)\\r\\n/, "$1")
+          .trim();
+        for (let i = 0; i < forLoopArr.length; i++) {
+          forLoopStrResult += forLoopContent.replace(
+            new RegExp(`{{${forLoopVars[1]}}}`, "g"),
+            forLoopArr[i]
+          );
+        }
+
+        renderedStr = renderedStr.replace(
+          new RegExp(
+            `{{for ${forLoopVars[1]} in ${forLoopVars[2]}}}(.*){{endfor}}`,
+            "s"
+          ),
+          forLoopStrResult
+        );
+      }
+
       return callback(null, renderedStr);
     })
     .catch(err => callback(err, null));
@@ -34,7 +60,11 @@ server.set("view engine", "eng.html");
 server.use(express.static(path.join(__dirname, "static")));
 
 server.get("/", (req, res) => {
-  return res.render("index", { name: "king", email: "mail@mail.com" });
+  return res.render("index", {
+    name: "king",
+    email: "mail@mail.com",
+    jobs: ["CEO", "Dev"]
+  });
 });
 
 server.listen(8000, () => console.log("Server running..."));
